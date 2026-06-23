d2aa548378911ba804590be2217234049b8981db
James Phelps <60262392+jphelps871@users.noreply.github.com>
2026-06-23 11:07:10 +0100

    Quick update to marker schema

--- lib/schemas/marker.ts
+++ lib/schemas/marker.ts
@@ -3,7 +3,7 @@ import { CATEGORIES } from "@/prisma/seeders/categories";
 
 export const registerMarkerSchema = z.object({
   description: z.string().min(5, "Must be longer than 5 characters.").max(300, "Cannot be longer than 300 characters."),
-  category: z.string(), // Handle wrong category from server
+  category: z.string("Must be a valid category"), // Handle wrong category from server
   lat: z.string("No location selected."),
   lng: z.string("No location selected."),
   dateOfNoise: z.date().refine(
